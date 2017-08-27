a2f1eead2edf770b33044676dc5f0cd886af5f6d
Alex Lapinski <contact@alexlapinski.name>
2017-08-26 22:55:27 -0400

    Added photon style template

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ let win;
 function createWindow() {
     win = new BrowserWindow({width: 800, height: 600});
 
-    win.load(url.format({
+    win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
     }));
 
-    win.webContents.openDevTools();
+    // win.webContents.openDevTools();
 
     win.on('closed', () => {
        win = null;
