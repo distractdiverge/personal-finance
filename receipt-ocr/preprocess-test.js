95ddc47e5d2c0ed5837936030b855323677b002e
Alex Lapinski <contact@alexlapinski.name>
2017-09-11 21:42:54 -0400

    Added resize script

--- receipt-ocr/preprocess-test.js
+++ receipt-ocr/preprocess-test.js
@@ -1,18 +1,34 @@
 const fs = require('fs');
 const path = require('path');
+const Promise = require('bluebird');
 const sharp = require('sharp');
 
+Promise.promisifyAll(fs);
 
 main();
 
 function main() {
-  const inputDir = path.join(__dirname, 'test', 'input');
-  const outputDir = path.join(__dirname, 'test', 'output');
+  const inputDir = path.join(__dirname, 'images', 'input');
+  const outputDir = path.join(__dirname, 'images', 'output');
+  const upscaleFactor = 2;
 
-  const inputFilepath = path.join(__dirname, 'test', filename);
-  const outputFilepath = path.join(__dirname, 'test' , `formatted_${filename}`);
-
-  sharp(inputFilepath)
-    .resize(200)
-    .toFile(outputFilepath);
+  // Create output directory if it doesn't exist.
+  fs.accessAsync(outputDir, fs.constants.W_OK)
+    .catch(err => fs.mkdirAsync(outputDir))
+    .then(() => fs.readdirAsync(inputDir))
+    .map(filename => ({
+      input: path.join(inputDir, filename),
+      output: path.join(outputDir, `formatted_${filename}`)
+    }))
+    .map(filepath => {
+      const image = sharp(filepath.input);
+      return image
+        .metadata()
+        .then(filedata => image.resize(Math.ceil(filedata.width * upscaleFactor)))
+        .then(newImage => newImage.toFile(filepath.output))
+        .then(fileinfo => ({info: fileinfo, paths: filepath}));
+    })
+    .map(result => {
+      console.log(`Resized '${result.paths.output}' to '${result.info.width}x${result.info.height}'`);
+    });
 }
