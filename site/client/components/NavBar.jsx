5069b8379924921825bd5a44dd69ce2ccafab918
Alex Lapinski <contact@alexlapinski.name>
2017-10-07 22:50:44 -0400

    Added missing dependency, added favicon, leaned up navigation bar

--- site/client/components/NavBar.jsx
+++ site/client/components/NavBar.jsx
@@ -9,18 +9,16 @@ const propTypes = {
 const render = ({props}) => {
   return (
     <nav className="pt-navbar pt-dark">
-      <div style={{margin: "0 auto",  width: "480px"}}>
-        <div className="pt-navbar-group pt-align-left">
-          <div className="pt-navbar-heading">{props.title}</div>
-        </div>
-        <div className="pt-navbar-group pt-align-right">
-          <button className="pt-button pt-minimal pt-icon-home">Home</button>
-          <button class="pt-button pt-minimal pt-icon-document">Files</button>
-          <span className="pt-navbar-divider"></span>
-          <button className="pt-button pt-minimal pt-icon-user"></button>
-          <button className="pt-button pt-minimal pt-icon-notifications"></button>
-          <button className="pt-button pt-minimal pt-icon-cog"></button>
-        </div>
+      <div className="pt-navbar-group pt-align-left">
+        <div className="pt-navbar-heading">{props.title}</div>
+      </div>
+      <div className="pt-navbar-group pt-align-right">
+        <button className="pt-button pt-minimal pt-icon-home">Home</button>
+        <button className="pt-button pt-minimal pt-icon-document">Files</button>
+        <span className="pt-navbar-divider"></span>
+        <button className="pt-button pt-minimal pt-icon-user"></button>
+        <button className="pt-button pt-minimal pt-icon-notifications"></button>
+        <button className="pt-button pt-minimal pt-icon-cog"></button>
       </div>
     </nav>
   );
--- site/client/index.js
+++ site/client/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { FocusStyleManager } from "@blueprintjs/core";
 import App from './components/App.jsx';
+
 import 'normalize.css/normalize.css';
 import '@blueprintjs/core/dist/blueprint.css';
+import './main.css';
 
+FocusStyleManager.onlyShowFocusOnTabs();
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
 
